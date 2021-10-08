299abcb9e417a6f45719bb649b0bb77c2c25a65a
aminatef <48862934+aminatef@users.noreply.github.com>
2021-10-08 13:40:57 +0200

    Create logEntry.java

---
+++ logEntry.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class logEntry {
+    private String ipAddress;
+    private Date accessTime;
+    private String request;
+    private int statusCode;
+    private int bytesReturned;
+    public logEntry(String ip,Date time,String req,int status,int bytes){
+        ipAddress = ip;
+        accessTime = time;
+        request = req;
+        statusCode = status;
+        bytesReturned = bytes;
+    }
+    public String getipAddress(){
+        return ipAddress;
+    }
+    public Date getAccessTime(){
+        return accessTime;
+    }
+    public String getRequest(){
+        return request;
+    }
+    public int getStatusCode(){
+        return statusCode;
+    }
+    public int getBytesReturned(){
+        return bytesReturned;
+    }
+    public String toString(){
+        return ipAddress + " " + accessTime+ " " +request+" "+statusCode + " "+ bytesReturned;
+    }
+}
